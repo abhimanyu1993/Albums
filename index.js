793fb1a8ce3c571297ca64a9fb3ab3d7832a224f
Abhimanyu Tyagi <atyagi@buffalo.edu>
2018-11-14 15:11:51 -0600

    Albums React Native first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => {
+  return (
+    <View>
+      <Header headerText={"Albums"} />
+      <AlbumList />
+    </View>
+  );
+};
+
+
+
+AppRegistry.registerComponent('auth', ()=>App);
